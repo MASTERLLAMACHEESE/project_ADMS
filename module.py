f7e2c4604fa10b5a94566d46fceab3ef6d5f12f1
Kaupo <sinimaakaupo@gmail.com>
2020-12-02 20:39:55 +0200

    led mapping changes

--- module.py
+++ module.py
@@ -19,10 +19,10 @@ pressed_key = (None, None)
 pos_in_seq = []
 pad_clicked = False
 
-seq_led = 0
-pitch_led = 1
-playback_led = 2
-seq_leds = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+seq_led = 16
+pitch_led = 17
+playback_led = 18
+seq_leds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 
 def current_time():
