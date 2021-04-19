3d5609157255c464b5f5115c2554707bbe88ed1b
Karl-Friedrich Lind <karl.f.lind@gmail.com>
2021-04-19 20:44:51 +0300

    test fixes

--- adms.py
+++ adms.py
@@ -19,17 +19,33 @@ def check_pressed_key():
     Get pressed key value from module and to something with it
     """
 
-    gui_class = gui_seq()
+    pad_mapping = {
+        # Pad keys
+        4:"Kick 1",
+        3:"Kick 2",
+        2:"Kick 3",
+        1:"Kick 4",
+        8:"Snare 1",
+        7:"Snare 2",
+        6:"Snare 3",
+        5:"Clap 1",
+        12:"Closed Hat 1",
+        11:"Closed Hat 2",
+        10:"Closed Hat 3",
+        9:"Closed Hat 4",
+        16:"Closed hat 5",
+        15:"Crash 1",
+        14:"Tom 1",
+        13:"Floot Tom 1"
+    }
 
     value, btn_type = module.pressed_key
     if btn_type == 'seq':
-        gui_class.seq_value(value)
         if module.selected:
             sequencer.toggle_sound(value-1, module.sounds[module.selected-1])  # add or remove sound
 
     if btn_type == 'pad':
         module.selected = value  # selected sound value
-        gui_class.pad_value(value)
         for i, beat in enumerate(module.sounds_in_beat):  # checks if sound is already in sequencer
             if module.sounds[value-1] in beat:
                 module.pos_in_seq.append(i)
@@ -51,6 +67,13 @@ def check_pressed_key():
         print(f'{module.current_time()} Playback {"enabled" if module.playback else "disabled"}')
         time.sleep(0.3)
     clear_pressed_key()
+
+    for e in pad_mapping:
+            if e not in module.gui_list[module.pos_in_seq]:
+                if len(module.gui_list[module.pos_in_seq]) < 4:
+                    module.gui_list[module.pos_in_seq].append(e)
+            else:
+                module.gui_list[module.pos_in_seq].remove(e)
 
 
 def pc_init():
@@ -93,44 +116,3 @@ pad.load_sounds()  # ToDo maybe this need to happen on Pad() init?
 
 while True:
     check_pressed_key()
-
-
-class gui_seq():
-    """
-    Create data for gui sequencer
-    """
-
-    pad_mapping = {
-        # Pad keys
-        4:"Kick 1",
-        3:"Kick 2",
-        2:"Kick 3",
-        1:"Kick 4",
-        8:"Snare 1",
-        7:"Snare 2",
-        6:"Snare 3",
-        5:"Clap 1",
-        12:"Closed Hat 1",
-        11:"Closed Hat 2",
-        10:"Closed Hat 3",
-        9:"Closed Hat 4",
-        16:"Closed hat 5",
-        15:"Crash 1",
-        14:"Tom 1",
-        13:"Floot Tom 1"
-    }
-
-    def pad_value(self, value):
-        self.pad_btn = value
-    
-    def seq_value(self, value):
-        self.seq_btn = value
-
-    def create_list(self):
-        #create list to store in seq_btn place, pat_btn values word
-        for e in gui_seq.pad_mapping:
-            if e not in module.gui_list[module.pos_in_seq]:
-                if len(module.gui_list[module.pos_in_seq]) < 4:
-                    module.gui_list[module.pos_in_seq].append(e)
-            else:
-                module.gui_list[module.pos_in_seq].remove(e)
