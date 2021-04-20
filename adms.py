ae0eb9ae1de807fd82f1513a050e98a6b00aafde
Karl-Friedrich Lind <karl.f.lind@gmail.com>
2021-04-20 17:19:59 +0300

    hotfix 2 v2

--- adms.py
+++ adms.py
@@ -74,11 +74,11 @@ def check_pressed_key():
         for e in pad_mapping:
             print (module.beat_in_seq)
             print (e)
-                if e not in module.gui_list[module.pos_in_seq[module.beat_in_seq]]:
-                    if len(module.gui_list[module.pos_in_seq[module.beat_in_seq]]) < 4:
-                        module.gui_list[module.pos_in_seq[module.beat_in_seq]].append(e)
-                else:
-                    module.gui_list[module.pos_in_seq[module.beat_in_seq]].remove(e)
+            if e not in module.gui_list[module.pos_in_seq[module.beat_in_seq]]:
+                if len(module.gui_list[module.pos_in_seq[module.beat_in_seq]]) < 4:
+                    module.gui_list[module.pos_in_seq[module.beat_in_seq]].append(e)
+            else:
+                module.gui_list[module.pos_in_seq[module.beat_in_seq]].remove(e)
         seq_temp_check = 0
 
 
